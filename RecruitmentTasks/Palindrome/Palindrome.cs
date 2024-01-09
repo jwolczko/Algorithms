d3e591b035b7d8ca5d60a2318298d125324119a1
Jakub Woczko <jakub.wolczko@outlook.com>
2024-01-09 19:18:42 +0100

    Palindrome

---
+++ RecruitmentTasks/Palindrome/Palindrome.cs
@@ -0,0 +1,14 @@
+﻿namespace Palindrome;
+
+public static class Palindrome
+{
+    public static bool IsPalindrome(string value)
+    {
+        if(string.IsNullOrEmpty(value)) return true;
+
+        string valueWithoutSpaces = value.Replace(" ", String.Empty);
+        string reversedValue = new string(valueWithoutSpaces.Reverse().ToArray());
+
+        return valueWithoutSpaces.Equals(reversedValue, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
