685a7a0d80afb3dacccbbc0f4b9c3978b2ad4afd
Jakub Woczko <jakub.wolczko@outlook.com>
2024-01-09 18:10:16 +0100

    Validation brackets

---
+++ RecruitmentTasks/BracketTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BracketTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ RecruitmentTasks/Brackets/Brackets.cs
@@ -0,0 +1,38 @@
+﻿namespace Brackets
+{
+    public static class Brackets
+    {
+        public static bool IsCorrect(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return false;
+
+            Stack<char> brackets = new();
+            Dictionary<char, char> avalaibleBrackets = new()
+            {
+                { '{','}' },
+                { '[',']' },
+                { '(',')' },
+                { '<','>' }
+            };
+
+            foreach(char character in value)
+            {
+                if (avalaibleBrackets.ContainsKey(character))
+                {
+                    brackets.Push(character);
+                }
+                else if (avalaibleBrackets.ContainsValue(character))
+                {
+                    if (brackets.Count == 0) continue;
+
+                    if (character == avalaibleBrackets[brackets.First()])
+                    {
+                        brackets.Pop();
+                    }
+                }
+            }
+
+            return brackets.Count == 0;
+        }
+    }
+}
