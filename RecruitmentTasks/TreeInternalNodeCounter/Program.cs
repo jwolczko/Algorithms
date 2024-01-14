a7d695cbefb0c64c2764dbe311380031f8f29346
Jakub Woczko <jakub.wolczko@outlook.com>
2024-01-14 18:03:11 +0100

    Graphs task from Codility

---
+++ RecruitmentTasks/TreeInternalNodeCounter/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TreeInternalNodeCounter;
+internal static class Program
+{
+    static void Main()
+    {
+        int[] treeArray = [ 1, 3, 1, -1, 3 ];
+        int result = TreeInternalNodeCounter.CountInternalNodes(treeArray);
+
+        Console.WriteLine($"Number of internal nodes: {result}");
+    }
+}
